ce158299db4120b20cd7e8a08554c226bf84eb1d
Daniel Rodowicz <daniel@dripa.net>
2026-04-24 20:59:42 -0400

    increase time font size on emery

--- src/c/main.c
+++ src/c/main.c
@@ -552,17 +552,37 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(s_window_layer);
 
   // set custom font
-  s_info_font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
-  int info_padding = 10;
-  int info_height = 28;
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TALLBOLD_49));
-  int time_padding = 2;
-  int time_height = 49;
-  s_date_font = s_info_font;
-  int date_padding = info_padding;
-  int date_height = info_height;
-  s_bt_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DRIPICONS_16));
-  s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_WEATHERICONS_18));
+  int info_padding;
+  int info_height;
+  int time_padding;
+  int time_height;
+  int date_padding;
+  int date_height;
+  #if defined(PBL_PLATFORM_EMERY)
+    s_info_font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+    info_padding = 10;
+    info_height = 28;
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TALLBOLD_64));
+    time_padding = 2;
+    time_height = 64;
+    s_date_font = s_info_font;
+    date_padding = info_padding;
+    date_height = info_height;
+    s_bt_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DRIPICONS_16));
+    s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_WEATHERICONS_18));
+  #else
+    s_info_font = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
+    info_padding = 10;
+    info_height = 28;
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TALLBOLD_49));
+    time_padding = 2;
+    time_height = 49;
+    s_date_font = s_info_font;
+    date_padding = info_padding;
+    date_height = info_height;
+    s_bt_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_DRIPICONS_16));
+    s_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_WEATHERICONS_18));
+  #endif
 
   // Position the time + date block
   int date_y = (bounds.size.h / 16) - date_padding;
