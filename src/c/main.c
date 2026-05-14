18783ba1ceb67e1846dd06ba214414691b0a127c
drod0258 <danrodowicz@gmail.com>
2026-05-14 08:50:10 -0400

    update message inbox_size
    increase message inbox_size to accommodate the additional color options

--- src/c/main.c
+++ src/c/main.c
@@ -1161,8 +1161,8 @@ static void init() {
   app_message_register_outbox_sent(outbox_sent_callback);
 
   // Open AppMessage
-  const int inbox_size = 256;
-  const int outbox_size = 256;
+  const int inbox_size = 384;
+  const int outbox_size = 128;
   app_message_open(inbox_size, outbox_size);
 }
 
